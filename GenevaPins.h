f9a9cd5bc76b61dc7e87d36ddd4235fdb0d4eb05
Drue Satterfield <drscp5@mst.edu>
2018-04-26 15:55:39 -0500

    Turns out we might not have an encoder. Lol mechanicals

--- GenevaPins.h
+++ GenevaPins.h
@@ -16,6 +16,5 @@ const uint8_t GENEVAMOTOR_INBPIN = PA_7;
 const uint8_t GENEVAMOTOR_PWMPIN = PK_4;
 const uint8_t GENEVAMOTOR_GEN = 3;
 const uint8_t GENEVA_LIM_PIN = PK_1;
-const uint8_t GENEVA_ENCODER_PIN = 0; //todo: fix dis
 
 #endif /* DRILLPINS_H_ */
