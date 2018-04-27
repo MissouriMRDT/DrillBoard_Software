18e48772a46f0528675670a208b432b5f3551075
Drue Satterfield <drscp5@mst.edu>
2018-04-27 14:05:23 -0500

    made everything open loop control for now

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ uint32_t screwDestination = 0;
 VNH5019 drillDriver(DRILLMOTOR1_GEN, DRILLMOTOR1_PWMPIN, DRILLMOTOR1_INAPIN, DRILLMOTOR1_INBPIN, false);
 VNH5019 screwDriver(SCREWMOTOR1_GEN, SCREWMOTOR1_PWMPIN, SCREWMOTOR1_INAPIN, SCREWMOTOR1_INBPIN, false);
 VNH5019 genevaDriver(GENEVAMOTOR_GEN, GENEVAMOTOR_PWMPIN, GENEVAMOTOR_INAPIN, GENEVAMOTOR_INBPIN, false);
-Ma3Encoder12b encoder(ENCODERGEN, ENCODERPIN);
-PIAlgorithm alg(AlgK, AlgI, AlgDt, &encoder);
 
 SingleMotorJoint drillInterface(InputPowerPercent, &drillDriver);
 SingleMotorJoint screwInterface(InputPowerPercent, &screwDriver);
@@ -34,15 +32,6 @@ void setup()
   roveComm_Begin(ip[0], ip[1], ip[2], ip[3]);
 
   loopTimer = setupTimer(7, TimerPeriodicInterrupt, AlgDt * 1000000, closedLoopHandler);
-
-  for(int i = 0; i < 20; i++)
-  {
-    encoder.getFeedback();
-    delay(4);
-  }
-
-  positionCounterBlock = rovePermaMem_useBlock(0, 255);
- // rovePermaMem_WriteBlockByte(positionCounterBlock, 0, 0); //remember to comment this out after the first  flash
 }
 
 void processBaseStationCommands()
@@ -64,18 +53,8 @@ void processBaseStationCommands()
         moveScrew(*((int16_t*)commandData));
         break;
 
-      case ScrewPositionId:
-      {
-        //for some reason TI's compilers throw a shitfit about float pointers unless you
-        //do it this way.
-        volatile float *data = (float*)commandData;
-        volatile float temp = data[0];
-        setScrewDestination(temp);
-        break;
-      }
-
       case GenevaId:
-        moveGeneva(*((int8_t*)commandData));
+        moveGeneva(*((int16_t*)commandData));
         break;
 
       default:
@@ -114,71 +93,21 @@ void moveScrew(int16_t moveValue)
   screwInterface.runOutputControl(moveValue);
 }
 
-void moveGeneva(int8_t Position)
+void moveGeneva(int16_t moveValue)
 {
-  if(Position > 5)
-  {
-      return;
-  }
-
-  uint8_t temp;
-  rovePermaMem_ReadBlockByte(positionCounterBlock, 0, &temp);
-
-  int8_t currentPosition = temp;
-
-  int movePosition = Position-currentPosition;
-
-  for(int i = 0; i < abs(movePosition); i++)
-  {
-    genevaMovePos(sign(movePosition));
-    currentPosition += 1*sign(movePosition);
-
-    if(currentPosition < 0)
-    {
-      currentPosition = 5;
-    }
-    else if(currentPosition > 5)
-    {
-      currentPosition = 0;
-    }
-
-    rovePermaMem_WriteBlockByte(positionCounterBlock, 0, currentPosition);
-  }
-}
-
-void genevaMovePos(int8_t Direction)
-{
-  genevaInterface.runOutputControl(1000*Direction);
-
- // while(checkLimSwitchHit(GENEVA_LIM_PIN));
- // while(!checkLimSwitchHit(GENEVA_LIM_PIN));
-
-  genevaInterface.runOutputControl(0);
+  genevaInterface.runOutputControl(moveValue);
 }
 
 void switchToClosedLoopScrew()
 {
-  screwInterface.switchModules(InputPosition, &alg);
-  screwDestination = encoder.getFeedback();
   startTimer(loopTimer);
   currentControlState = ClosedLoop;
 }
 
 void switchToOpenLoopScrew()
 {
-  screwInterface.removeIOConverter(InputPowerPercent);
   stopTimer(loopTimer);
   currentControlState = OpenLoop;
-}
-
-void setScrewDestination(float degreesDestination)
-{
-  if(currentControlState != ClosedLoop)
-  {
-    switchToClosedLoopScrew();
-  }
-
-  screwDestination = degreesDestination * DEGREES_TO_POS;
 }
 
 bool checkLimSwitchHit(uint8_t pin)
--- main.h
+++ main.h
@@ -22,8 +22,7 @@ void processBaseStationCommands();
 void setup();
 void moveDrill(int16_t moveVal);
 void moveScrew(int16_t moveValue);
-void moveGeneva(int8_t Position);
-void genevaMovePos(int8_t Direction);
+void moveGeneva(int16_t Position);
 void switchToClosedLoopScrew();
 void switchToOpenLoopScrew();
 void closedLoopHandler();
@@ -32,8 +31,7 @@ void setScrewDestination(float degreesDestination);
 
 const uint16_t DrillId = 0xB54;
 const uint16_t ScrewId = 0xB55;
-const uint16_t ScrewPositionId = 0xB56;
-const uint16_t GenevaId = 1809;
+const uint16_t GenevaId = 0xB56;
 
 const uint8_t ip[4] = {192, 168, 1, 139};
 const uint8_t AlgK = 100;
